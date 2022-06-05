5e4509127e31b3a53113744bf29f5be92f286ae0
allmysmarts <rockid332@gmail.com>
2022-06-06 00:03:47 +0900

    update match-betting

--- contracts/MatchBetting.sol
+++ contracts/MatchBetting.sol
@@ -3,14 +3,17 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MatchBetting is Ownable{
+contract MatchBetting is Ownable {
     struct StakerInfo {
-        uint256 goalsSelection;
+        uint256 goalPairId;
         uint256 amount;
+        uint256 withdrawn;
     }
 
-    struct GoalsSelection {
+    struct GoalPair {
+        /// @dev number of staked members
         uint256 totalStakers;
+        /// @dev total amount of staked value
         uint256 totalAmount;
     }
 
@@ -30,20 +33,29 @@ contract MatchBetting is Ownable{
     /// @dev minimum stake amount
     uint256 public minStakeAmount;
 
+    /// @dev final match result
+    uint256 public finalResultOfGoalPair;
+
     /// @dev User's staked amount, and goals
     mapping(address => StakerInfo) public staked;
 
     /// @dev Goals selection details with total staked users count, and amount
-    mapping(uint256 => GoalsSelection) public goalsSelected;
+    mapping(uint256 => GoalPair) public goalPairsList;
 
     /// @dev number of users staked already
     uint256 public totalStakedUsers;
+
+    /// @dev total amount of betted
+    uint256 public totalStakedAmount;
 
     /// @dev multiplier of odds rate, to mitigate rounding loss
     uint256 public constant ODDS_MULTIPLIER = 1e5;
 
     /// @dev maximum number of goals that any team can get in a single match
     uint256 public constant MAXIMUM_GOALS = 100;
+
+    /// @dev minimum amount required to bet
+    uint256 public constant MINIMUM_STAKE_AMOUNT = 5 ether;
 
     constructor(
         string memory referenceURI_,
@@ -63,44 +75,106 @@ contract MatchBetting is Ownable{
 
     /// @dev stake with anticipated goals of two teams
     function stake(uint256 team1Goals, uint256 team2Goals) external payable beforeKickOff {
-        require(team1Goals != team2Goals, "Draw not allowed");
         require(team1Goals < MAXIMUM_GOALS && team2Goals < MAXIMUM_GOALS, "Invalid goals");
+        require(team1Goals != team2Goals, "Draw not allowed");
+        require(msg.value >= MINIMUM_STAKE_AMOUNT, "Too small amount");
 
-        StakerInfo storage userInfo = staked[msg.sender];
-        require(userInfo.amount == 0, "Not allowed multiple staking");
+        StakerInfo storage stakerInfo = staked[msg.sender];
+        require(stakerInfo.amount == 0, "Not allowed multiple staking");
 
-        uint256 goalsSelection = team1Goals * MAXIMUM_GOALS + team2Goals;
-        GoalsSelection storage goalsInfo = goalsSelected[goalsSelection];
+        uint256 goalPairId = getGoalId(team1Goals, team2Goals);
+        GoalPair storage goalPair = goalPairsList[goalPairId];
 
-        userInfo.goalsSelection = goalsSelection;
-        userInfo.amount = msg.value;
-        goalsInfo.totalStakers ++;
-        goalsInfo.totalAmount += msg.value;
+        stakerInfo.goalPairId = goalPairId;
+        stakerInfo.amount = msg.value;
+
+        goalPair.totalStakers ++;
+        goalPair.totalAmount += msg.value;
+
+        totalStakedUsers ++;
+        totalStakedAmount += msg.value;
     }
 
     function restake(uint256 team1Goals, uint256 team2Goals) external payable beforeKickOff {
-        require(team1Goals != team2Goals, "Draw not allowed");
         require(team1Goals < MAXIMUM_GOALS && team2Goals < MAXIMUM_GOALS, "Invalid goals");
+        require(team1Goals != team2Goals, "Draw not allowed");
 
+        StakerInfo storage stakerInfo = staked[msg.sender];
+        require(stakerInfo.amount > 0, "Not staked already");
+
+        uint256 oldGoalPairId = stakerInfo.goalPairId;
+        uint256 stakedAmount = stakerInfo.amount;
+
+        uint256 newGoalPairId = getGoalId(team1Goals, team2Goals);
+        require(oldGoalPairId != newGoalPairId, "Same as before");
+
+        stakerInfo.goalPairId = newGoalPairId;
+
+        GoalPair storage oldGoalPair = goalPairsList[oldGoalPairId];
+        oldGoalPair.totalAmount -= stakedAmount;
+        oldGoalPair.totalStakers --;
+
+        GoalPair storage newGoalPair = goalPairsList[newGoalPairId];
+        newGoalPair.totalAmount += stakedAmount;
+        newGoalPair.totalStakers ++;
     }
 
     /// @dev withdraw win amount after win
     function withdraw() external afterFinish {
+        require(finalResultOfGoalPair != 0, "Not determined result");
 
+        StakerInfo storage stakerInfo = staked[msg.sender];
+        require(stakerInfo.amount > 0, "Not staked already");
+        require(stakerInfo.goalPairId == finalResultOfGoalPair, "Not winner");
+
+        GoalPair storage goalPair = goalPairsList[finalResultOfGoalPair];
+
+        uint256 rewardsAmount = totalStakedAmount * stakerInfo.amount * 9 / goalPair.totalAmount / 10;
+        uint256 withdrawnAmount = rewardsAmount - stakerInfo.withdrawn;
+
+        stakerInfo.withdrawn += withdrawnAmount;
+
+        payable(msg.sender).transfer(withdrawnAmount);
     }
 
-    function getGoalId(uint256 team1Goals, uint256 team2Goals) external pure returns (uint256) {
-        return team1Goals * MAXIMUM_GOALS + team2Goals;
-    }
+    /// @dev returns potential win amount of staker who already staked
+    function odds(address staker) public view beforeKickOff returns (uint256) {
+        StakerInfo storage stakerInfo = staked[staker];
+        uint256 stakedAmount = stakerInfo.amount;
 
-    /// @dev returns potential win amount of user who already staked
-    function odds(address user) public view returns (uint256) {
+        require(stakedAmount > 0, "Not staked already");
 
+        GoalPair storage goalPair = goalPairsList[stakerInfo.goalPairId];
+
+        /// @dev 10% of staked amount will be returned to treasury, for the purpose of nft-staking dividends
+        uint256 estimatedRewards = address(this).balance * stakedAmount * 9 / goalPair.totalAmount / 10;
+
+        return estimatedRewards * ODDS_MULTIPLIER / stakedAmount;
     }
 
     /// @dev estimate odds for upcoming staking activity
-    function estimateOdds(uint256 team1Goals, uint256 team2Goals, uint256 stakeAmount) external view returns (uint256) {
+    function estimateOdds(
+        uint256 team1Goals, 
+        uint256 team2Goals, 
+        uint256 stakeAmount
+    ) external view beforeKickOff returns (uint256) {
+        require(team1Goals < MAXIMUM_GOALS && team2Goals < MAXIMUM_GOALS, "Invalid goals");
+        require(team1Goals != team2Goals, "Draw not allowed");
+        require(stakeAmount >= MINIMUM_STAKE_AMOUNT, "Too small amount");
 
+        uint256 goalPairId = getGoalId(team1Goals, team2Goals);
+        GoalPair storage goalPair = goalPairsList[goalPairId];
+
+        /// @dev 10% of staked amount will be returned to treasury, for the purpose of nft-staking dividends
+        uint256 estimatedRewards = 
+            (address(this).balance + stakeAmount) * stakeAmount * 9 / 
+            (goalPair.totalAmount + stakeAmount) / 10;
+
+        return estimatedRewards * ODDS_MULTIPLIER / stakeAmount;
+    }
+
+    function getGoalId(uint256 team1Goals, uint256 team2Goals) public pure returns (uint256) {
+        return team1Goals * MAXIMUM_GOALS + team2Goals;
     }
 
 
@@ -110,7 +184,7 @@ contract MatchBetting is Ownable{
     }
 
     modifier afterFinish() {
-        require(block.timestamp >= kickOffTime + 60 * 60 * 24, "Not finished");
+        require(block.timestamp >= kickOffTime + 60 * 60 * 48, "Not finished");
         _;
     }
 }
